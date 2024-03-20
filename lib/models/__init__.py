ef98c282e47e609e663e4d7032e9cab718d514cd
Rotich <jane.rotich@student.moringaschool.com>
2024-03-20 11:44:35 +0300

    fix database

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,3 +2,24 @@ import sqlite3
 
 CONN = sqlite3.connect('visitors.db')
 CURSOR = CONN.cursor()
+# CREATE TABLE companies (
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(30),
+#     description VARCHAR(300)
+# );
+
+# CREATE TABLE visitors (
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(30),
+#     id_no VARCHAR(30),
+#     time_in DATETIME,
+#     time_out DATETIME,
+#     reason_for_visit VARCHAR(300),
+#     car_plate VARCHAR(20)
+# );
+
+# CREATE TABLE company_visitors (
+#     visitor_id INTEGER REFERENCES visitors(id),
+#     company_id INTEGER REFERENCES companies(id),
+#     PRIMARY KEY (visitor_id, company_id)
+# );
